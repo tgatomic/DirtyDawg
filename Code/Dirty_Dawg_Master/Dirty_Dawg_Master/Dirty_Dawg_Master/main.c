207e27260233cc0f57325662908c15cf160cdc39
Christoffer Bask <chrisse_b@hotmail.com>
2016-04-18 14:03:06 +0200

    Added codeproject for AtmelStudio

---
+++ Code/Dirty_Dawg_Master/Dirty_Dawg_Master/Dirty_Dawg_Master/main.c
@@ -0,0 +1,18 @@
+/*
+ * Dirty_Dawg_Master.c
+ *
+ * Created: 2016-04-18 13:32:02
+ * Author : Atomic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
